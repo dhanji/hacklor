368b3fc2b8979d06546b4f52d8e20957e18aff27
Dhanji R. Prasanna <>
2021-05-11 04:50:12 +0000

    Initial commit

---
+++ main.hs
@@ -0,0 +1,12 @@
+-- Median of two sorted lists
+
+median ls
+  | length ls `mod` 2 == 0    = fromIntegral ((ls !! mid) + (ls !! mid - 1)) / 2
+  | otherwise                 = fromIntegral $ ls !! mid
+  where
+      mid = (length ls) `div` 2
+
+
+
+
+main = putStrLn $ show $ median [1, 2, 3, 4]
