de5c2c8a446b997350688065943089023a5f4af1
Dhanji R. Prasanna <>
2021-05-11 23:44:15 +0000

    Merge two lists and apply median (final form)

--- main.hs
+++ main.hs
@@ -1,13 +1,21 @@
 -- Median of two sorted lists
 
-median [] = 0
-median ls
+med [] = 0
+med ls
   | length ls `mod` 2 == 0    = fromIntegral ((ls !! mid) + (ls !! mid - 1)) / 2
   | otherwise                 = fromIntegral $ ls !! mid
   where
       mid = (length ls) `div` 2
 
 
+merge [] ls = ls
+merge ls [] = ls
+merge (x:xs) (y:ys)
+  | x == y    = x: y: merge xs ys
+  | x < y     = x: merge xs (y:ys)
+  | otherwise = y: merge (x:xs) ys
 
 
-main = putStrLn $ show $ median [1, 2, 3, 4]
+median l1 l2 = med $ merge l1 l2
+
+main = putStrLn $ show $ median [0, 0] [0, 0]
